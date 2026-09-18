08758423c6c96383ea6efcd8f0354923f352fdb3
Jableed43 <javiernehuenlopez@gmail.com>
2026-09-18 19:22:34 -0300

    Add ESLint configuration and initial index.js file with JSON example
    - Created eslint.config.js for configuring ESLint with Expo settings.
    - Added index.js containing a JavaScript object example and comments on JSON usage.

---
+++ 5/index.js
@@ -0,0 +1,24 @@
+// json -> JavaScript Object Notation
+
+const obj = {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz",
+      phone: "1-770-736-8031 x56442",
+      website: "hildegard.org"
+    }
+
+    // Todos los tipos de js tienen metodos nativos
+    // Strings, boolean, numbers, obj, arrays, etc.. tienen sus propios metodos
+    // si vos tenes el dato en JSON, tambien tiene metodos pero no los que queremos
+
+    // Los objs de js pueden tener bucles aplicados, condicionales, metodos nativos
+
+// los JSON sirven para transportar informacion a través de la red
+
+// js nos provee metodos nativos para aplicar sobre nuestros datos, para procesar la informacion
+
+// parse -> de JSON a js
+// stringify -> js -> JSON
+
